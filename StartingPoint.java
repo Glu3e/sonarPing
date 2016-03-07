b081d2486130d27030cbd1d99b92645a1fc90be9
pyojoon <tpotm9@gmail.com>
2016-03-07 14:13:03 -0500

    Added files via upload

---
+++ StartingPoint.java
@@ -0,0 +1,8 @@
+
+public class StartingPoint {
+	public static void main(String [] args){
+		SonarPingModel model = new SonarPingModel("COM3");	
+		SonarPingView view = new SonarPingView();
+		SonarPingController controller = new SonarPingController(model, view,null);
+	}
+}
