a816fd9413d907a63b9ce4c0e465199679aa38e9
pyojoon <tpotm9@gmail.com>
2016-03-18 13:15:33 -0400

    Added files via upload

--- src/MongoControl.java
+++ src/MongoControl.java
@@ -1,7 +1,8 @@
-
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
 import javax.swing.JOptionPane;
 
 public class MongoControl {
@@ -22,6 +23,61 @@ public class MongoControl {
 		this.theView.addMongoButtonListener(new MongoButtonListener());		
 	}
 	
+	public Boolean validate()
+	{
+		Boolean rt=false;
+		//String EMAIL_REGEX = "^[\\w-_\\.+]*[\\w-_\\.]\\ @([\\w]+\\.)+[\\w]+[\\w]$";
+		
+		
+		if( theView.firstName.getText().isEmpty() ) 
+		{
+			JOptionPane.showMessageDialog(null,"You need to input first name!");
+			rt = false;
+		}
+		else if( theView.lastName.getText().isEmpty() ) 
+		{
+			JOptionPane.showMessageDialog(null,"You need to input last name!");
+			rt = false;
+		}
+		else if( theView.password.getText().isEmpty() ) 
+		{
+			JOptionPane.showMessageDialog(null,"You need to input password!");
+			rt = false;
+		}
+		else if( theView.email.getText().isEmpty() ) 
+		{
+			JOptionPane.showMessageDialog(null,"You need to input email!");
+			rt = false;
+		}
+		else if(!theView.email.getText().isEmpty())
+		{
+			try {
+				InternetAddress emailAddr = new InternetAddress(theView.email.getText());
+				emailAddr.validate();
+			} catch (AddressException ex) {
+				JOptionPane.showMessageDialog(null,"You need to input a correct email!");
+				rt = false;
+			}
+			//return result;
+		}
+		
+		else if( theView.type.getText().isEmpty() ) 
+		{
+			JOptionPane.showMessageDialog(null,"You need to input type!");
+			rt = false;
+		}
+		else if ( !theView.type.getText().equals("DEPENDENT") )
+		{
+			JOptionPane.showMessageDialog(null,"You need to input type to be DEPENDENT!");
+			rt = false;
+		}
+		else
+		{
+			rt = true;
+		}
+		return rt;
+	}
+	
 	public void clearFields(){
 		theView.firstName.setText("");
 		theView.lastName.setText("");
@@ -36,8 +92,9 @@ public class MongoControl {
 		public void actionPerformed(ActionEvent e) 
 		{
 			String str;
-			int rt;
+			int dialogreturn;
 			String key, value;
+			Boolean ret;
 			
 			firstName = theView.getFirstName();
 			lastName = theView.getLastName();
@@ -49,7 +106,9 @@ public class MongoControl {
 			{
 				if(e.getActionCommand().equals("CreateButton"))
 				{
-					theModel.insertMongoDB(firstName,lastName,email,password,type);
+					ret = validate();
+					if(ret)
+						theModel.insertMongoDB(firstName,lastName,email,password,type);
 				}
 				else if(e.getActionCommand().equals("deleteButton"))
 				{
@@ -62,12 +121,12 @@ public class MongoControl {
 				}
 				else if(e.getActionCommand().equals("browseButton"))
 				{
-					str = theModel.browseMongoDB();
-					JOptionPane.showMessageDialog(null,str);
+					str = theModel.browseMongoDB(theView, theModel);
+					//JOptionPane.showMessageDialog(null,str);
 				}	
 				else if(e.getActionCommand().equals("removeallButton"))
 				{
-					rt = theModel.removeallMongo();
+					dialogreturn = theModel.removeallMongo();
 					JOptionPane.showMessageDialog(null,"Remove all the data in MongoDB");	
 				}				
 				
