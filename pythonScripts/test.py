729c367b1815e0bd1d68bd603ec364d3ae69b3d6
SonarBoy <Joshua.L99.C@gmail.com>
2017-04-10 16:19:30 -0400

    Working Python Script

--- pythonScripts/test.py
+++ pythonScripts/test.py
@@ -1,3 +1,5 @@
+# Authors: Daniel Kerkreswell, Joshua Collaco
+
 import bpy
 import json
 
@@ -5,23 +7,26 @@ from pprint import pprint
 
 with open('C:\\Users\\Jupiter\\Desktop\\Javascript\\Work\\sonarPing\\pythonScripts\\strings.json') as data_file:    
     data = json.load(data_file)
-pprint(data)
+
+pprint(len(data))
+
+for i in range(len(data)):
+	pprint(i)
+
+	mesh = bpy.data.meshes.new("Cube")
+	verts = [(data[i]['pointx'], data[i]['pointy'],data[i]['pointz'])]
+
+	object = bpy.data.objects.new("Cube", mesh)
+
+	bpy.context.scene.objects.link(object)
+
+	mesh.from_pydata(verts,[],[])
+	mesh.update(calc_edges=True)
 
 
-#Define vertices and faces (replace these test values with input arrays later, this code will create a cube)
-verts = [(data[0]['pointx'], data[0]['pointy'],data[0]['pointz'])]
-faces = [(0,1,2,3), (4,5,6,7), (0,4,5,1), (1,5,6,2), (2,6,7,3), (3,7,4,0)]
+	
+	
+
+	
 
 
-
-#Define mesh and object
-mesh = bpy.data.meshes.new("Cube")
-object = bpy.data.objects.new("Cube", mesh)
- 
-#Set location and scene of object
-object.location = bpy.context.scene.cursor_location
-bpy.context.scene.objects.link(object)
- 
-#Create mesh
-mesh.from_pydata(verts,[],faces)
-mesh.update(calc_edges=True)
